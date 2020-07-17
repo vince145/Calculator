ffb54fb78eb059808e0c53033e881e663bdcf106
vince145 <48505084+vince145@users.noreply.github.com>
2020-07-17 17:19:30 -0500

    added auth for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Calculator from './Calculator/Calculator';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <Calculator></Calculator>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
