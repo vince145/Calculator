99e912e40fdc603f8693cd6af3e2603a855f1220
vince145 <48505084+vince145@users.noreply.github.com>
2020-07-17 15:55:11 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './Calculator/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator></Calculator>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Calculator/Calculator.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Calculator.css';
+import * as math from 'mathjs';
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calcInput: '',
+      calculations: [],
+      error: null,
+    };
+  }
+
+  // Handles users typing into form
+  handleChange = event => {
+    this.setState({calcInput: event.target.value});
+  };
+
+  // Handles users submitting a calculation by
+  // using enter or clicking on the submit button
+  handleSubmit = event => {
+    // Try and catch handle invalid input for mathjs evaluate
+    this.setState({error: false});
+    if (this.state.calcInput) {
+      try {
+        math.evaluate(this.state.calcInput)
+      } catch (error) {
+        this.setState({error});
+        throw(error);
+      }
+      if (!this.state.error) {
+        this.state.calculations.unshift(this.state.calcInput);
+        // Handles limiting shown calculations to only 10 calculations
+        this.state.calculations.length = 
+          this.state.calculations.length <= 10 ?
+          this.state.calculations.length : 10;
+        this.setState({calcInput: ''});
+      }
+    }
+    event.preventDefault();
+    event.target.reset();
+  };
+
+  // Lists the last 10 calculations performed and their
+  // answers
+  CalculationList() {
+    var calculations = this.state.calculations;
+    var results = calculations.map((calculation) =>
+    (calculation + ' = ' + math.evaluate(calculation)));
+    var listResults = results.map((result, index) =>
+      <li key={index}> 
+        {result}
+      </li>
+    );
+    return (
+      <ul>{listResults}</ul>
+    )
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="Calculator">
+          <form onSubmit={this.handleSubmit}>
+            <div className="Form">
+              <label>
+                  <input
+                    type='text' 
+                    value={this.state.calcInput}
+                    onChange={this.handleChange}
+                  />
+                </label>
+            </div>
+            <div className="Submit">
+              <input
+                type="submit"
+                value="Submit"
+              />
+            </div>
+          </form>
+          <h4>{this.CalculationList()}</h4>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
